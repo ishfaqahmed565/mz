c4a11266febdc156145925f6e3137a37710c857a
oblivion <ahmedishfaq737@gmail.com>
2023-05-05 01:43:56 +0600

    Background of top navbar fixed

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -2,9 +2,7 @@
 	<section class="background2 dark:bg-gray-900">
 		<div class="">
 			<header class="max-w-6xl mx-auto">
-				<nav
-					class="bg-transparent border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800"
-				>
+				<nav class="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
 					<div
 						class="pb-2 border-b-[1px] border-gray-800 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
 					>
